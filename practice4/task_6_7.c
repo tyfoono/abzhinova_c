fdc939761c70bf37c32405bad665df435490c14b
tyfoono <aabzhinova@ya.ru>
2026-03-26 12:05:07 +0300

    practice4

--- practice4/task_6_7.c
+++ practice4/task_6_7.c
@@ -2,7 +2,12 @@
 
 int main(){
     int a = 10, b = 5;
-    int *ptr_a = &b, **ptr_b = &ptr_a;
+    int *ptr_a, **ptr_b;
+
+    ptr_a = &a;
+    ptr_b = &ptr_a;
+
+    
 
     printf("%p %p\n", ptr_a, ptr_b);
     printf("%d\n", **ptr_b);
